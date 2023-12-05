73989b9199b205aacceb6c93bf19f764c656dc2d
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-12-05 14:17:03 -0300

    add: react hook form

--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -10,6 +10,7 @@ export const SearchFormContainer = styled.form`
     background: ${props => props.theme["gray-900"]};
     color: ${props => props.theme["gray-300"]};
     padding: 1rem;
+    
     &::placeholder {
       color: ${props => props.theme["gray-500"]};
     }
@@ -25,7 +26,13 @@ export const SearchFormContainer = styled.form`
     color: ${props => props.theme["green-300"]};
     font-weight: bold;
     border-radius: 6px;
-    &:hover {
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+    &:not(:disabled):hover {
       background: ${props => props.theme["green-500"]};
       border-color: ${props => props.theme["green-500"]};
       color: ${props => props.theme["white"]};
